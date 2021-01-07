7cd189ef46d097cd39fef6fad801855914a07800
Rushik Bhalodia <rushik8698@gmail.com>
2021-01-07 16:59:44 +0530

    Updates

---
+++ precache-manifest.cad2f476c96ba284620d6e8fe74ec3d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06db32f2a7360550eb50a33deb350746",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "78bbaeb2eecb6f99c5c8",
+    "url": "/TodoList/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "0c07632b5b8d1c16f1ec",
+    "url": "/TodoList/static/css/main.2e17c9ab.chunk.css"
+  },
+  {
+    "revision": "78bbaeb2eecb6f99c5c8",
+    "url": "/TodoList/static/js/2.e0c11cb2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/TodoList/static/js/2.e0c11cb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c07632b5b8d1c16f1ec",
+    "url": "/TodoList/static/js/main.23320179.chunk.js"
+  },
+  {
+    "revision": "23d45432894410375589",
+    "url": "/TodoList/static/js/runtime-main.d0f80f2c.js"
+  }
+]);
